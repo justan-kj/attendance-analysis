72c77b87b41e77d58013a196f75fce6b7d0e3162
justan-kj <jtjtjt703@gmail.com>
2025-06-30 20:17:15 +0100

    Streamlined typing for ExcelParser

--- src/utils/ExcelParser.tsx
+++ src/utils/ExcelParser.tsx
@@ -5,24 +5,29 @@ export interface ExcelTable {
     workbookName: string
     worksheetName: string
     headers: string[]
-    rows: unknown[][]
+    rows: Record<string, unknown>[]
 }
 
 export const parseExcelWorksheet = (
     workbookName: string,
     worksheet: WorkSheet
 ): ExcelTable => {
-    const json = utils.sheet_to_json(worksheet)
-    const headers = new Set<string>()
-    json.forEach((row) => {
-        Object.keys(row as object).forEach((colName) => {
-            headers.add(colName)
-        })
-    })
+    const json = utils.sheet_to_json<Record<string, unknown>>(worksheet)
+    if (json.length === 0) {
+        return {
+            workbookName,
+            worksheetName: worksheet.name,
+            headers: [],
+            rows: [],
+        }
+    }
+
+    const headers = Object.keys(json[0])
+
     return {
         workbookName,
         worksheetName: worksheet.name,
-        headers: Array.from(headers),
-        rows: json.slice(1).map((row) => Object.values(row as object)),
+        headers,
+        rows: json,
     }
 }
