7594b3b5d132c7c65ee9d54e0bb6c48271e93a78
justan-kj <jtjtjt703@gmail.com>
2025-07-01 00:54:45 +0100

    Added aggregation function to DataProcessing

--- src/utils/DataProcessing.tsx
+++ src/utils/DataProcessing.tsx
@@ -1,4 +1,6 @@
-export type FilterMode =
+import _ from 'lodash'
+
+type FilterMode =
     | 'equals'
     | 'notEquals'
     | 'contains'
@@ -8,34 +10,124 @@ export type FilterMode =
     | 'lessThan'
     | 'lessThanOrEqual'
 
+export type ColumnFilter = {
+    column: string
+    mode: FilterMode
+    value: number | string
+}
+
+type AggegationMode = 'sum' | 'mean' | 'max' | 'min'
+
+export type ColumnAggregation = {
+    groupByColumn: string
+    valueColumn: string
+    mode: AggegationMode
+}
+
 export const filterRowsByColumn = (
     rows: Record<string, unknown>[],
-    column: string,
-    mode: FilterMode,
-    value: number | string
+    dataFilter: ColumnFilter
 ): Record<string, unknown>[] => {
-    switch (mode) {
+    switch (dataFilter.mode) {
         case 'equals':
-            return rows.filter((row) => row[column] === value)
+            return rows.filter(
+                (row) => row[dataFilter.column] === dataFilter.value
+            )
         case 'notEquals':
-            return rows.filter((row) => row[column] !== value)
+            return rows.filter(
+                (row) => row[dataFilter.column] !== dataFilter.value
+            )
         case 'contains':
             return rows.filter((row) =>
-                String(row[column]).includes(String(value))
+                String(row[dataFilter.column]).includes(
+                    String(dataFilter.value)
+                )
             )
         case 'notContains':
             return rows.filter(
-                (row) => !String(row[column]).includes(String(value))
+                (row) =>
+                    !String(row[dataFilter.column]).includes(
+                        String(dataFilter.value)
+                    )
             )
         case 'greaterThan':
-            return rows.filter((row) => Number(row[column]) > Number(value))
+            return rows.filter(
+                (row) =>
+                    Number(row[dataFilter.column]) > Number(dataFilter.value)
+            )
         case 'greaterThanOrEqual':
-            return rows.filter((row) => Number(row[column]) >= Number(value))
+            return rows.filter(
+                (row) =>
+                    Number(row[dataFilter.column]) >= Number(dataFilter.value)
+            )
         case 'lessThan':
-            return rows.filter((row) => Number(row[column]) < Number(value))
+            return rows.filter(
+                (row) =>
+                    Number(row[dataFilter.column]) < Number(dataFilter.value)
+            )
         case 'lessThanOrEqual':
-            return rows.filter((row) => Number(row[column]) <= Number(value))
+            return rows.filter(
+                (row) =>
+                    Number(row[dataFilter.column]) <= Number(dataFilter.value)
+            )
         default:
-            throw new Error(`Unknown filter mode: ${mode}`)
+            throw new Error(`Unknown filter mode: ${dataFilter.mode}`)
     }
+}
+
+export const aggegateRowsByColumn = (
+    rows: Record<string, unknown>[],
+    agg: ColumnAggregation
+): Record<string, unknown>[] => {
+    const groups = _.groupBy(rows, agg.groupByColumn)
+    const aggregatedRows: Record<string, unknown>[] = []
+
+    for (const groupKey in groups) {
+        const groupRows = groups[groupKey]
+        const hasNonNumbers = groupRows.some((row) => {
+            const value = row[agg.valueColumn]
+            return value === null || value === undefined || isNaN(Number(value))
+        })
+
+        if (hasNonNumbers) {
+            throw new Error(
+                `Column '${agg.valueColumn}' contains non-numeric values. All values must be numbers for aggregation.`
+            )
+        }
+
+        let aggregatedValue: number
+
+        switch (agg.mode) {
+            case 'sum':
+                aggregatedValue = _.sumBy(groupRows, agg.valueColumn)
+                break
+            case 'mean':
+                aggregatedValue = _.meanBy(groupRows, agg.valueColumn)
+                break
+            case 'max': {
+                const maxRecord = _.maxBy(groupRows, agg.valueColumn)
+                aggregatedValue = maxRecord
+                    ? Number(maxRecord[agg.valueColumn])
+                    : 0
+                break
+            }
+            case 'min': {
+                const minRecord = _.minBy(groupRows, agg.valueColumn)
+                aggregatedValue = minRecord
+                    ? Number(minRecord[agg.valueColumn])
+                    : 0
+
+                break
+            }
+            default:
+                throw new Error(`Unknown aggregation mode: ${agg.mode}`)
+        }
+
+        aggregatedRows.push({
+            [agg.groupByColumn]: groupKey,
+            [agg.valueColumn]: aggregatedValue,
+        })
+    }
+
+    return aggregatedRows
 }
