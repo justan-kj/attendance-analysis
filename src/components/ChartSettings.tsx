9277ac78b20addad4442d1792aba7f4a52f7e816
justan-kj <jtjtjt703@gmail.com>
2025-07-01 12:15:50 +0100

    Refactored Select into a reusable component

--- src/components/ChartSettings.tsx
+++ src/components/ChartSettings.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from 'react'
-import {
-    Paper,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Stack,
-    Button,
-    Typography,
-} from '@mui/material'
+import { Paper, Button, Typography, Stack } from '@mui/material'
 import type {
-    AggregationMode,
     ColumnAggregation,
+    AggregationMode,
 } from '../utils/DataProcessing'
+import AppSelect from './Select'
 
 export interface ChartSettingsProps {
     onSubmit: (settings: ColumnAggregation) => void
@@ -22,13 +14,13 @@ export interface ChartSettingsProps {
 const ChartSettings: React.FC<ChartSettingsProps> = ({ onSubmit, headers }) => {
     const [groupByColumn, setGroupByColumn] = useState<string>('Year of Course')
     const [valueColumn, setValueColumn] = useState<string>('% Attendance')
-    const [mode, setMode] = useState<AggregationMode>('mean')
+    const [mode, setMode] = useState<string>('mean')
 
     const handleSubmit = () => {
         onSubmit({
             groupByColumn,
             valueColumn,
-            mode,
+            mode: mode as AggregationMode,
         })
     }
     return (
@@ -41,62 +33,22 @@ const ChartSettings: React.FC<ChartSettingsProps> = ({ onSubmit, headers }) => {
                 <Typography variant="h6" gutterBottom>
                     Chart Settings
                 </Typography>
-                <FormControl
-                    fullWidth
-                    sx={{
-                        margin: 2,
-                    }}
-                >
-                    <InputLabel id="group-select-label">
-                        Group Column
-                    </InputLabel>
-                    <Select
-                        labelId="group-select-label"
-                        id="group-select"
-                        value={groupByColumn}
-                        label="Group Column"
-                        onChange={(e) => setGroupByColumn(e.target.value)}
-                    >
-                        {headers.map((header) => (
-                            <MenuItem key={header} value={header}>
-                                {header}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth>
-                    <InputLabel id="value-select-label">
-                        Value Column
-                    </InputLabel>
-                    <Select
-                        labelId="value-select-label"
-                        id="value-select"
-                        value={valueColumn}
-                        label="Value Column"
-                        onChange={(e) => setValueColumn(e.target.value)}
-                    >
-                        {headers.map((header) => (
-                            <MenuItem key={header} value={header}>
-                                {header}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <FormControl fullWidth>
-                    <InputLabel id="mode-select-label">Aggregation</InputLabel>
-                    <Select
-                        labelId="dmode-select-label"
-                        id="mode-select"
-                        value={mode}
-                        label="Aggregation"
-                        onChange={(e) => setMode(e.target.value)}
-                    >
-                        <MenuItem value={'sum'}>Sum</MenuItem>
-                        <MenuItem value={'mean'}>Mean</MenuItem>
-                        <MenuItem value={'max'}>Max</MenuItem>
-                        <MenuItem value={'min'}>Min</MenuItem>
-                    </Select>
-                </FormControl>
+                <AppSelect
+                    menuItems={headers}
+                    label="Group Column"
+                    onChange={setGroupByColumn}
+                />
+                <AppSelect
+                    menuItems={headers}
+                    label="Value Column"
+                    onChange={setValueColumn}
+                />
+                <AppSelect
+                    menuItems={['sum', 'mean', 'max', 'min']}
+                    label="Aggregation"
+                    onChange={setMode}
+                />
+
                 <Button
                     variant="contained"
                     color="primary"
---
+++ src/components/Select.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import type { SelectChangeEvent } from '@mui/material/Select'
+
+export interface AppSelectProps {
+    onChange: (value: string) => void
+    label: string
+    menuItems: string[]
+}
+
+const AppSelect: React.FC<AppSelectProps> = ({
+    onChange,
+    label,
+    menuItems,
+}) => {
+    const [selectedValue, setSelectedValue] = useState<string>()
+
+    const handleChange = (event: SelectChangeEvent) => {
+        const value = event.target.value
+        setSelectedValue(value)
+        onChange(value)
+    }
+    return (
+        <FormControl
+            fullWidth
+            sx={{
+                margin: 2,
+            }}
+        >
+            <InputLabel id="select-label">{label}</InputLabel>
+            <Select
+                labelId="select-label"
+                id="select"
+                value={selectedValue}
+                label={label}
+                onChange={handleChange}
+            >
+                {menuItems.map((item) => (
+                    <MenuItem key={item} value={item}>
+                        {item}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    )
+}
+
+export default AppSelect
